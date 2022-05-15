b996957ddcd961e846c3270a1c58973f51785ea4
name <you@example.com>
2022-05-15 12:28:26 +0200

    c

--- prisma/migrations/20220513104245_added_dropdown_reference/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "column_info" ADD COLUMN     "referenceToDropdownId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "column_info" ADD CONSTRAINT "column_info_referenceToDropdownId_fkey" FOREIGN KEY ("referenceToDropdownId") REFERENCES "dropdown"("id") ON DELETE SET NULL ON UPDATE CASCADE;
